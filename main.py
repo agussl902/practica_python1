74a3251d7ccc53871743259c44c531f25aaefd48
agustin silva <agustinsilva902@gmail.com>
2026-03-15 16:56:26 -0300

    subo el ejercicio 1 de la practica

---
+++ main.py
@@ -0,0 +1,5 @@
+anio = int(input ("Escribi tu Año de nacimiento: "))
+
+print("en el año que compliras 18 es:", anio+ 18)
+print("en el año que cumpliras 21 es:", anio+ 21)
+print("en el año que cumpliras 100 es:", anio+ 100)
