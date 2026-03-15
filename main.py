ea5c05ccc18bc71a7ee73e14f0651ee4702e3d6a
agustin silva <agustinsilva902@gmail.com>
2026-03-15 17:31:40 -0300

    elimino el main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-anio = int(input ("Escribi tu Año de nacimiento: "))
-
-print("en el año que compliras 18 es:", anio+ 18)
-print("en el año que cumpliras 21 es:", anio+ 21)
-print("en el año que cumpliras 100 es:", anio+ 100)
